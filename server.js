1e73c43f4f9427ac9681b91dec0cfe90ef0436e6
asitaru <andreisitaru93@gmail.com>
2017-03-20 15:31:24 +0200

    test

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 var express = require('express');
+var parser = require('ua-parser-js');
+
 
 var app = express();
 
-var useragent = require('express-useragent');
+//var useragent = require('express-useragent');
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(useragent.express())
+//app.use(useragent.express())
 
 app.get('/', (req,res)=>{
-    res.send(req.headers['x-forwarded-for']);
-    //res.send(req.useragent.source);
+    var ua = parser(req.headers['user-agent']);
+    res.end(JSON.stringify(req.headers['user-agent'].split(' ').slice(1,4).join(' ').slice(1,-1),null,' '))
+    //res.end(req.headers['user-agent'])
+    //res.send(req.headers['x-forwarded-for'] || req.connection.remoteAddress);
+
+    //res.send(JSON.stringify(req.headers['user-agent']));
 });
 
 app.listen(app.get('port'), () => {
