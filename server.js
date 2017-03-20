c5fc8a984987699138f7382514a44a25da8a2fc9
asitaru <andreisitaru93@gmail.com>
2017-03-20 14:12:39 +0200

    heroku fixes

--- server.js
+++ server.js
@@ -2,4 +2,17 @@ var express = require('express');
 
 var app = express();
 
+var useragent = require('express-useragent');
+
 app.set('port', (process.env.PORT || 5000));
+
+app.use(useragent.express())
+
+app.get('/', (req,res)=>{
+    res.send(req.ip);
+    //res.send(req.useragent.source);
+});
+
+app.listen(app.get('port'), () => {
+    console.log('App is running on port', app.get('port'));
+});
