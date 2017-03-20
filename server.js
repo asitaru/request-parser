e8ce0677f7a6efc87f4518ce711bb5a863537992
asitaru <andreisitaru93@gmail.com>
2017-03-20 20:21:06 +0200

    main object created

--- server.js
+++ server.js
@@ -11,12 +11,15 @@ app.set('port', (process.env.PORT || 5000));
 //app.use(useragent.express())
 
 app.get('/', (req,res)=>{
-    var ua = parser(req.headers['user-agent']);
-    res.end(JSON.stringify(req.headers['user-agent'].split(' ').slice(1,4).join(' ').slice(1,-1),null,' '))
-    //res.end(req.headers['user-agent'])
-    //res.send(req.headers['x-forwarded-for'] || req.connection.remoteAddress);
-
-    //res.send(JSON.stringify(req.headers['user-agent']));
+    var head = req.headers['user-agent'];
+    var software = head.slice(head.indexOf('(') + 1, head.indexOf(')'));
+    var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    var language;
+    res.send(JSON.stringify({
+        software: software,
+        ip: ip,
+        language: language
+    })); 
 });
 
 app.listen(app.get('port'), () => {
