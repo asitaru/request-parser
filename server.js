5d2f577c48df41cc165c1128311bc7437b344ed5
asitaru <andreisitaru93@gmail.com>
2017-03-21 15:32:12 +0200

    added language and finalised the project

--- server.js
+++ server.js
@@ -1,25 +1,26 @@
 var express = require('express');
-var parser = require('ua-parser-js');
 
 
 var app = express();
 
-//var useragent = require('express-useragent');
 
 app.set('port', (process.env.PORT || 5000));
-
-//app.use(useragent.express())
 
 app.get('/', (req,res)=>{
     var head = req.headers['user-agent'];
     var software = head.slice(head.indexOf('(') + 1, head.indexOf(')'));
     var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    var language;
-    res.send(JSON.stringify({
-        software: software,
-        ip: ip,
-        language: language
-    })); 
+    var lang = req.headers['accept-language'];
+    var language = lang.slice(0,lang.indexOf(';'));
+    if(language.indexOf(',') > -1) {
+        language = language.slice(0,language.indexOf(','));
+    }
+    var response = JSON.stringify({
+        ipaddress: ip,
+        language: language,
+        software: software
+    }, null, " ");
+    res.send('<p style="font-family: Arial, Helvetica, sans-serif">' + response + '</p>')
 });
 
 app.listen(app.get('port'), () => {
